de974dd6ba95e88c37bfbbd611d4dc527fbac7c0
eranbrown92 <eranbrown92@gmail.com>
2018-10-29 15:54:35 -0500

    date

--- src/components/VehicleOut.js
+++ src/components/VehicleOut.js
@@ -1,20 +1,24 @@
 import React from "react";
 
 class VehicleOut extends React.Component {
+  handleClick(e){
+    const user = document.getElementById('user').innerHTML
+    alert(`${user} returned a GOV at ` + Date.toString())
+  }
   render() {
     const { user } = this.props;
     return (
       <div>
         <ul className="collection with-header">
           <li className="collection-header light-blue">
-            <h4>{user.name}</h4>
+            <h4 id='user'>{user.name}</h4>
           </li>
           <li className="collection-item">Reason: {user.reason}</li>
           <li className="collection-item">Building: {user.building}</li>
           <li className="collection-item">
             <div>
               Time Out: {user.timeout}
-              <a href="#!" className="secondary-content">
+              <a  onClick={this.handleClick}href="#!" className="secondary-content">
                 <i className="red-text">Return</i>
               </a>
             </div>
