3a8cc038fc7e3ce3abef8b623c29a973c3339edb
eranbrown92 <eranbrown92@gmail.com>
2018-10-30 08:21:41 -0500

    added row to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,16 @@ import React from "react";
 const Header = props => {
   const { branding } = props;
   return (
-    <nav>
-      <div className="nav-wrapper light-blue">
-        <a href="#!" className="brand-logo center">
-          {branding}
-        </a>
-      </div>
-    </nav>
+    <div className='row'>
+      <nav>
+        <div className="nav-wrapper light-blue">
+          <a href="#!" className="brand-logo center">
+            {branding}
+          </a>
+        </div>
+      </nav>
+    </div>
+    
   );
 };
 export default Header;
