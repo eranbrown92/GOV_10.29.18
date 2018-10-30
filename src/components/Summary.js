44188abf98576dd11c8b161108417006a0d6eade
eranbrown92 <eranbrown92@gmail.com>
2018-10-30 13:30:01 -0500

    added available GOv counter

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -7,14 +7,15 @@ class Summary extends React.Component {
     super();
     this.state = {
       details: [],
-      availGov: 2
+      availGov: 1
     };
     this.handleFormData = this.handleFormData.bind(this);
   }
   handleFormData(e) {
-    this.setState({
-      details: [...this.state.details, e]
-    });
+    this.setState(()=>({
+      details: [...this.state.details, e],
+      availGov: this.state.availGov - 1
+    }));
   }
   returnFromJob(id) {
     // copy the state
@@ -22,18 +23,21 @@ class Summary extends React.Component {
     // remove the id from the state.details array
     stateCopy.splice(id, 1);
     // update state
-    this.setState({ details: stateCopy });
+    this.setState({ details: stateCopy, availGov: this.state.availGov + 1 });
     console.log(id, ' returned at ', Date())
   }
   render() {
     return (
       <div>
         <div>
-          <CheckoutForm handleFormData={this.handleFormData} />
+          <CheckoutForm 
+          handleFormData={this.handleFormData} 
+          availGov={this.state.availGov}
+          />
         </div>
         <div className="col s9">
             <div className="nav-wrapper center">
-              <div className="chip">Available GOV: {this.state.availGov}</div>
+            <div className="chip">{this.state.availGov <= 0 ? 'Sorry, there are no available GOVs' : `${this.state.availGov} available GOV(s)` }</div>
             </div>
           {this.state.details.map((user, id) => {
             return (
